ea328064eb5bc00e118376189bebedd5cebaa928
mosa.abedelhady@mail.huji.ac.il <51644794+mosaabed@users.noreply.github.com>
2020-12-29 17:22:28 +0200

    Done

--- src/App.js
+++ src/App.js
@@ -1,37 +1,63 @@
 import React, { Component } from 'react';
+//import { Route } from 'react-router-dom';
 import './App.css';
+import { BrowserRouter as Router, Route ,Link} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Fentities from './components/Fentities'
+
+
+class Fentity extends Component {
+  render() {
+      const { fentities, name } = this.props.match.params
+      const fentity = this.props.state[fentities].filter(f => {
+          return f.name.toLowerCase() === name.toLowerCase()
+      })[0]
+      console.log(fentity);
+      return (
+          <div id="creature-container"><h1>{fentity.name}</h1><img src={fentity.imgUrl} alt="" /><div className="title">Power:</div><div className="power text"> {fentity.power}</div><div className="other text">{fentity.other}</div></div>
+      )
+  }
+}
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      wizards: [
-        { name: "Merlin", power: "Wisdom", other: "Helped King Arthur", imgUrl: "https://tinyurl.com/merlin-image" },
-        { name: "Morgana Le Fay", power: "Forces of Nature", other: "Trapped Merlin in a cave for eternity", imgUrl: "https://tinyurl.com/morgana-image" },
-        { name: "Gandalf", power: "Plot Convenience", other: "Once broke a bridge", imgUrl: "https://tinyurl.com/gandalf-img" }
-      ],
-      bestiary: [
-        { name: "Smaug", power: "Fire and Flying", other: "Burned a city to with his mouth", imgUrl: "https://tinyurl.com/smaug-image" },
-        { name: "Buckbeak", power: "Flying", other: "Knocked over Malfoy like a boss", imgUrl: "https://tinyurl.com/buckbeak-image" },
-        { name: "Cerebrus", power: "Having three heads", other: "Holding back the dead since 100 BCE", imgUrl: "https://tinyurl.com/cerebrus-image" }
-      ]
-    }
+constructor() {
+  super()
+  this.state = {
+    wizards: [
+      { name: "Merlin", power: "Wisdom", other: "Helped King Arthur", imgUrl: "https://tinyurl.com/merlin-image" },
+      { name: "Morgana Le Fay", power: "Forces of Nature", other: "Trapped Merlin in a cave for eternity", imgUrl: "https://tinyurl.com/morgana-image" },
+      { name: "Gandalf", power: "Plot Convenience", other: "Once broke a bridge", imgUrl: "https://tinyurl.com/gandalf-img" }
+    ],
+    bestiary: [
+      { name: "Smaug", power: "Fire and Flying", other: "Burned a city to with his mouth", imgUrl: "https://tinyurl.com/smaug-image" },
+      { name: "Buckbeak", power: "Flying", other: "Knocked over Malfoy like a boss", imgUrl: "https://tinyurl.com/buckbeak-image" },
+      { name: "Cerebrus", power: "Having three heads", other: "Holding back the dead since 100 BCE", imgUrl: "https://tinyurl.com/cerebrus-image" }
+    ]
   }
-  
-  render() {
-    const state = this.state
-    return (
+}
+
+
+render() {
+  const state = this.state
+  return (
+    <Router>
       <div className="App">
         <div id="home-background"></div>
         <div id="main-links">
-          {/* Main Links */}
-        </div>
-        {/* Routes go here v */}
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
 
-        {/* Routes go here ^ */}
+
+        </div>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" exact render={() => <About items={Object.keys(state)} />} />
+          <Route path="/directory/:fentities" exact render={({ match }) => <Fentities match={match} state={state} />}/>
+          <Route path="/directory/:fentities/:name" exact render={({match}) => <Fentity match={match} state={state}/>} />
       </div>
-    );
-  }
+    </Router>
+  );
+}
 }
 
 export default App;
